d88e727b31ae020e75fafb311f271ce5dd7b268d
Thibaud <152966050+Sibleouf@users.noreply.github.com>
2024-01-12 09:55:36 +0100

    Add files via upload

---
+++ dns.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+#Lance les mises à jour
+apt update -y && apt upgrade -y > /dev/nell 2>&1
+
+#Lance l'installation des outils pour le service DNS
+apt install -y bind9 sudo > /dev/nell 2>&1
+
+#Ajout de thibaud dans le groupe sudo
+gpasswd -a thibaud sudo
+
+#Configuration du adresse IP fixe
+echo "# The loopback network interface
+auto lo
+iface lo inet loopback
+
+# The primary network interface
+allow-hotplug eth0
+iface eth0 inet static
+address 192.168.0.103
+netmask 255.255.255.0 
+gateway 192.168.0.1
+dns-nameservers 192.168.0.103 8.8.8.8
+dns-domain beesafe.co " > /etc/network/interfaces
+systemctl restart networking
+echo "Redémarrage carte réseau"
+echo "IP = $(hostname -I)"
+
+#Configuration des options global de Bind
+echo "options {
+        directory "/var/cache/bind";
+
+        forwarders {
+            8.8.8.8;
+            8.8.4.4;
+        };
+
+        dnssec-validation auto;
+
+        auth-nxdomain no;    # conform to RFC1035
+        listen-on-v6 { any; };
+};
+" > /etc/bind/named.conf.options
+
+#Création d'un fichier de zone pour le domaine beesafe.co
+echo "$TTL    604800
+@       IN      SOA     www.beesafe.co. admin.beesafe.co. (
+                     2021122801         ; Serial
+                         604800         ; Refresh
+                          86400         ; Retry
+                        2419200         ; Expire
+                         604800 )       ; Negative Cache TTL
+;
+@       IN      NS      www.beesafe.co.
+@       IN      A       192.168.0.103
+serveur IN      A       192.168.0.103
+" > /etc/bind/zones/beesafe.co
+
+#Ajouter une référence à votre fichier de zone
+echo "zone "beesafe.co" {
+    type master;
+    file "/etc/bind/zones/db.beesafe.co";
+};
+
+zone "0.168.192.in-addr.arpa" {
+    type master;
+    file "/etc/bind/zones/db.192.168.0";
+};
+" > /etc/bind/named.conf.local
+
+#Création d'une zone inversée
+
+echo "$TTL    604800
+@       IN      SOA     www.beesafe.co. admin.beesafe.co. (
+                 2021122801         ; Serial
+                     604800         ; Refresh
+                      86400         ; Retry
+                    2419200         ; Expire
+                     604800 )       ; Negative Cache TTL
+;
+@       IN      NS      www.beesafe.co.
+103     IN      PTR     www.beesafe.co." > /etc/bind/zones/db.192.168.0
+
+#Redémarrage du service bind
+systemctl restart named.service
+
+#Modification de résolution de DNS
+echo "domain numericable.fr
+search numericable.fr
+nameserver 192.168.0.103" > /etc/resolv.conf
+
+
+
+
+
+
+
+
+
