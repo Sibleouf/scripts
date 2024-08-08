244533478e86dbd11f9ebff066882c03fc8e1d9f
Thibaud <152966050+Sibleouf@users.noreply.github.com>
2024-08-08 14:14:39 +0200

    SSH keys scirpt.ps1

--- ssh keys scirpt.ps1
+++ ssh keys scirpt.ps1
@@ -1,15 +1,25 @@
 # Demander à l'utilisateur de renseigner les variables
 $nom = Read-Host -Prompt "Entrez le nom de la clef"
-$utilisateur = Read-Host -Prompt "Entrez le nom d'utilisateur pour la connexion SSH"
+$utilisateur = Read-Host -Prompt "Entrez le nom d'rasputilisateur pour la connexion SSH"
 $adresseIP = Read-Host -Prompt "Entrez l'adresse IP du serveur distant"
 
 # Générer la paire de clés sur l'ordinateur client
 ssh-keygen -t ed25519 -a 256 -f "$env:USERPROFILE\.ssh\$nom"
 
-# Copier la clé publique sur le serveur distant
+# Lire le contenu de la clé publique
 $contenuClePublique = Get-Content "$env:USERPROFILE\.ssh\$nom.pub"
-$commande = "echo '$contenuClePublique' >> ~/.ssh/authorized_keys"
-ssh $utilisateur@$adresseIP $commande
+
+# Préparer la commande à exécuter sur le serveur distant
+$commandeSetup = @"
+mkdir -p ~/.ssh && \
+touch ~/.ssh/authorized_keys && \
+chmod 700 ~/.ssh && \
+chmod 600 ~/.ssh/authorized_keys && \
+echo '$contenuClePublique' >> ~/.ssh/authorized_keys
+"@ -replace "`r", ""
+
+# Exécuter la commande sur le serveur distant en une seule connexion SSH
+ssh $utilisateur@$adresseIP $commandeSetup
 
 # Rajouter la nouvelle configuration au fichier de configuration SSH
 $cheminConfig = "$env:USERPROFILE\.ssh\config"
@@ -19,7 +29,3 @@ Host $nom
     User $utilisateur
     IdentityFile "$env:USERPROFILE\.ssh\$nom"
 "@ | Out-File -FilePath $cheminConfig -Append -Encoding utf8
-
-
-
-
